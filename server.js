4bcfb2770bca00bd4f1fe72eaf000dd74f12378a
Ojas Gulati <ojasgulati100@gmail.com>
2019-10-22 13:43:59 +0530

    kk

--- server.js
+++ server.js
@@ -3,19 +3,26 @@ const { errorCodes, onSuccessfullyFound } = require("./config");
 const Person = require("./models/Person");
 const express = require("express");
 const bodyParser = require("body-parser");
+var cors = require("cors");
+
 var app = express();
 
 const PORT = 3001;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post("/searchForNumberPlate", async (req, res) => {
   try {
     if (!req.body.vehicleNo) throw new Error(errorCodes.NUMBER_PLATE_MISSING);
     let person = await Person.findOne({ vehicleNo: req.body.vehicleNo });
-    if (!person) throw new Error(errorCodes.NUMBER_PLATE_NOT_FOUND);
+    if (!person) {
+      console.log("ojas");
+      throw new Error(JSON.stringify(errorCodes.NUMBER_PLATE_NOT_FOUND));
+    }
     res.status(200).send(onSuccessfullyFound(person));
   } catch (e) {
+    console.log(e);
     res.status(404).send(e.message);
   }
 });
